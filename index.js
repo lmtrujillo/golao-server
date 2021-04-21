ea657f5c689d9f51cd5515d8ba6b2c3003383eb8
Luis Mario <luis.mario.trujillo@live.com.mx>
2021-04-20 21:15:31 -0500

    Added useful comments to functions and added new function

--- index.js
+++ index.js
@@ -3,19 +3,23 @@ const fetch = require("node-fetch");
 
 const api_key_token = process.env.API_TOKEN_PARAM + process.env.API_KEY;
 
+// GET ALL THE LEAGUES
 function getLeagues() {
+    var soccerLeagues;
     var endpoint = process.env.API_URL + "leagues" + api_key_token;
 
     fetch(endpoint)
         .then(response => response.json())
         .then((res) => {
-            console.log(res.data);
+            soccerLeagues = res.data;
+            console.log(soccerLeagues);
         })
         .catch((error) => {
             console.error('Error:', error);
         });
 }
 
+// GET N LEAGUE
 function getLeague(league_id) {
     var endpoint = process.env.API_URL + "leagues/" + league_id + api_key_token;
 
@@ -29,8 +33,8 @@ function getLeague(league_id) {
         });
 }
 
-
-function getNWeekFromLeague(n_week, league_id) {
+// GET N WEEK MATCHES FROM N LEAGUE
+function getNWeekMatchesFromLeague(n_week, league_id) {
     var seasons = [];
     var rounds = [];
     var fixtures = [];
@@ -84,8 +88,37 @@ function getNWeekFromLeague(n_week, league_id) {
         });
 }
 
+// GET ALL WEEKS FROM N LEAGUE
+function getWeeksFromNLeague(league_id) {
+    var weeks;
+    var seasons_endpoint = process.env.API_URL + "seasons" + api_key_token;
+    fetch(seasons_endpoint)
+        .then(response => response.json())
+        .then((res) => {
+            seasons = res.data;
+            // Find current_season_id from current season from N league_id
+            var current_season_id = seasons.find(x => (x.is_current_season === true &&  x.league_id === league_id)).id
+
+                // Getting all rounds from current seasons
+                rounds_endpoint = process.env.API_URL + "rounds/season/" + current_season_id + api_key_token;
+                fetch(rounds_endpoint)
+                    .then(response => response.json())
+                    .then((res) => {
+                        weeks = res.data;
+                        console.log(weeks);
+                    })
+                    .catch((error) => {
+                        console.error('Error:', error);
+                    });
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+}
+
 // API TEST CALLS:
 
 //getLeagues();
 //getLeague(501);
-//getNWeekFromLeague(2, 271);
+//getNWeekMatchesFromLeague(2, 271);
+getWeeksFromNLeague(271);
