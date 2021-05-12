4b793380eb9587c8c64277d38f9fd2b6d6c85178
Luis Mario <luis.mario.trujillo@live.com.mx>
2021-05-12 11:15:24 -0500

    Finished matches tests\

--- tests/matchesTests/matches.test.js
+++ tests/matchesTests/matches.test.js
@@ -1,5 +1,5 @@
-import { getMatch, getMatchesTimeframe } from '../../api/matchesApi.js';
-import { getMatchExpected, getMatchesTimeframeExpected } from './matchesData.js'
+import { getMatch, getMatchesTimeframe, getMatchesFromNWeekLeague, getMatchesTimeframeTeam } from '../../api/matchesApi.js';
+import { getMatchExpected, getMatchesTimeframeExpected, getMatchesFromNWeekLeagueExpected, getMatchesTimeframeTeamExpected } from './matchesData.js'
 
 
 test('getMatch endpoint', async () => {
@@ -11,4 +11,16 @@ test('getMatch endpoint', async () => {
 test('getMatchesTimeframe endpoint', async () => {
     const data = await getMatchesTimeframe('2020-09-19', '2020-09-21');
     expect(data).toStrictEqual(getMatchesTimeframeExpected);
+});
+
+test('getMatchesFromNWeekLeague endpoint', async () => {
+    const data = await getMatchesFromNWeekLeague(5, 271);
+    expect(data[0]).toStrictEqual(getMatchesFromNWeekLeagueExpected);
+    expect(data.length).toEqual(11);
+});
+
+test('getMatchesTimeframeTeam endpoint', async () => {
+    const data = await getMatchesTimeframeTeam('2020-09-19', '2020-09-21', 939);
+    expect(data).toStrictEqual(getMatchesTimeframeTeamExpected);
+    expect(data.length).toEqual(1);
 });
--- tests/matchesTests/matchesData.js
+++ tests/matchesTests/matchesData.js
@@ -1280,3 +1280,165 @@ export const getMatchesTimeframeExpected =  [
       is_placeholder: false
     }
   ]
+
+export const getMatchesFromNWeekLeagueExpected = {
+  id: 16475344,
+  league_id: 501,
+  season_id: 17141,
+  stage_id: 77447501,
+  round_id: 194978,
+  group_id: null,
+  aggregate_id: null,
+  venue_id: 8909,
+  referee_id: 17265,
+  localteam_id: 53,
+  visitorteam_id: 62,
+  winner_team_id: 62,
+  weather_report: {
+    code: 'clouds',
+    type: 'broken clouds',
+    icon: 'https://cdn.sportmonks.com/images/weather/04d.png',
+    temperature: { temp: 50.32, unit: 'fahrenheit' },
+    temperature_celcius: { temp: 10.2, unit: 'celcius' },
+    clouds: '75%',
+    humidity: '76%',
+    pressure: 1027,
+    wind: { speed: '6.93 m/s', degree: 70 },
+    coordinates: { lat: 55.87, lon: -4.26 },
+    updated_at: '2020-10-17T13:15:14.327602Z'
+  },
+  commentaries: false,
+  attendance: null,
+  pitch: null,
+  details: null,
+  neutral_venue: false,
+  winning_odds_calculated: true,
+  formations: { localteam_formation: '3-5-2', visitorteam_formation: '4-3-3' },
+  scores: {
+    localteam_score: 0,
+    visitorteam_score: 2,
+    localteam_pen_score: null,
+    visitorteam_pen_score: null,
+    ht_score: '0-1',
+    ft_score: '0-2',
+    et_score: null,
+    ps_score: null
+  },
+  time: {
+    status: 'FT',
+    starting_at: {
+      date_time: '2020-10-17 11:30:00',
+      date: '2020-10-17',
+      time: '11:30:00',
+      timestamp: 1602934200,
+      timezone: 'UTC'
+    },
+    minute: 90,
+    second: null,
+    added_time: null,
+    extra_minute: null,
+    injury_time: null
+  },
+  coaches: { localteam_coach_id: 1467946, visitorteam_coach_id: 50 },
+  standings: { localteam_position: 2, visitorteam_position: 1 },
+  assistants: {
+    first_assistant_id: 15817,
+    second_assistant_id: 12772,
+    fourth_official_id: 15818
+  },
+  leg: '1/1',
+  colors: {
+    localteam: {
+      color: '#007848',
+      kit_colors: '#F0F0F0,#139343,#5B0623,#0A0A0A,#5C8FAE,#FFDF1B,#F0F0F0'
+    },
+    visitorteam: {
+      color: '#2B72DE',
+      kit_colors: '#2B72DE,#2B72DE,#2424FF,#2424FF,#365CBF,#365CBF,#F0F0F0'
+    }
+  },
+  deleted: false,
+  is_placeholder: false
+}
+
+export const getMatchesTimeframeTeamExpected = [
+  {
+    id: 16773962,
+    league_id: 271,
+    season_id: 17328,
+    stage_id: 77447994,
+    round_id: 199448,
+    group_id: null,
+    aggregate_id: null,
+    venue_id: 342,
+    referee_id: 15805,
+    localteam_id: 939,
+    visitorteam_id: 2650,
+    winner_team_id: 939,
+    weather_report: {
+      code: 'clouds',
+      type: 'broken clouds',
+      icon: 'https://cdn.sportmonks.com/images/weather/04d.png',
+      temperature: { temp: 64.11, unit: 'fahrenheit' },
+      temperature_celcius: { temp: 17.8, unit: 'celcius' },
+      clouds: '78%',
+      humidity: '77%',
+      pressure: 1023,
+      wind: { speed: '3.36 m/s', degree: 330 },
+      coordinates: { lat: 56.14, lon: 8.97 },
+      updated_at: '2020-09-19T15:15:28.332126Z'
+    },
+    commentaries: false,
+    attendance: null,
+    pitch: null,
+    details: null,
+    neutral_venue: false,
+    winning_odds_calculated: true,
+    formations: { localteam_formation: '4-2-3-1', visitorteam_formation: '4-3-3' },
+    scores: {
+      localteam_score: 1,
+      visitorteam_score: 0,
+      localteam_pen_score: null,
+      visitorteam_pen_score: null,
+      ht_score: '1-0',
+      ft_score: '1-0',
+      et_score: null,
+      ps_score: null
+    },
+    time: {
+      status: 'FT',
+      starting_at: {
+        date_time: '2020-09-19 13:30:00',
+        date: '2020-09-19',
+        time: '13:30:00',
+        timestamp: 1600522200,
+        timezone: 'UTC'
+      },
+      minute: 90,
+      second: null,
+      added_time: null,
+      extra_minute: null,
+      injury_time: null
+    },
+    coaches: { localteam_coach_id: 82974, visitorteam_coach_id: 32002797 },
+    standings: { localteam_position: 6, visitorteam_position: 8 },
+    assistants: {
+      first_assistant_id: 12811,
+      second_assistant_id: 39108,
+      fourth_official_id: 14811
+    },
+    leg: '1/1',
+    colors: {
+      localteam: {
+        color: '#0A0A0A',
+        kit_colors: '#0A0A0A,#0A0A0A,#0A0A0A,#0A0A0A,#5C8FAE,#FFDF1B,#0A0A0A'
+      },
+      visitorteam: {
+        color: '#F0F0F0',
+        kit_colors: '#F0F0F0,#F0F0F0,#FBED32,#00C1BA,#C40010,#0046A8,#F0F0F0'
+      }
+    },
+    deleted: false,
+    is_placeholder: false
+  }
+]
