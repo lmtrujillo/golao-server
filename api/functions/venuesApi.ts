791c381c0c77256c6dc016882ce072ef9e9a0cfa
Luis Mario <luis.mario.trujillo@live.com.mx>
2022-01-26 09:31:02 -0600

    Identations

--- api/functions/venuesApi.ts
+++ api/functions/venuesApi.ts
@@ -1,38 +1,39 @@
-const fetch = require('node-fetch');
-import dotenv from 'dotenv'
-dotenv.config()
-import Ajv from 'ajv'
-const ajv = new Ajv()
-import { venueSchema, venuesSchema } from '../../schemas/venuesSchemas'
+const fetch = require("node-fetch");
+import dotenv from "dotenv";
+dotenv.config();
+import Ajv from "ajv";
+const ajv = new Ajv();
+import { venueSchema, venuesSchema } from "../../schemas/venuesSchemas";
 
 const api_key_token = process.env.API_TOKEN_PARAM! + process.env.API_KEY!;
 
 // GET N VENUE
 export async function getVenue(venue_id: any): Promise<any> {
-    var endpoint = process.env.API_URL! + "venues/" + venue_id + api_key_token;
-    var venue = await fetch(endpoint, {method: 'GET'})
-        .then((response: any) => response.json())
-        .then((res: any) => {
-            return (ajv.validate(venueSchema, res.data) ? res.data : ajv.errors);
-        })
-        .catch((error: any) => {
-            return error;
-        });
+  var endpoint = process.env.API_URL! + "venues/" + venue_id + api_key_token;
+  var venue = await fetch(endpoint, { method: "GET" })
+    .then((response: any) => response.json())
+    .then((res: any) => {
+      return ajv.validate(venueSchema, res.data) ? res.data : ajv.errors;
+    })
+    .catch((error: any) => {
+      return error;
+    });
 
-    return venue;
+  return venue;
 }
 
 // GET VENUES FROM N SEASON
 export async function getVenuesSeason(season_id: any): Promise<any> {
-    var endpoint = process.env.API_URL! + "venues/season/" + season_id + api_key_token;
-    var venue = await fetch(endpoint, {method: 'GET'})
-        .then((response: any) => response.json())
-        .then((res: any) => {
-            return (ajv.validate(venuesSchema, res.data) ? res.data : ajv.errors);
-        })
-        .catch((error: any) => {
-            return error;
-        });
+  var endpoint =
+    process.env.API_URL! + "venues/season/" + season_id + api_key_token;
+  var venue = await fetch(endpoint, { method: "GET" })
+    .then((response: any) => response.json())
+    .then((res: any) => {
+      return ajv.validate(venuesSchema, res.data) ? res.data : ajv.errors;
+    })
+    .catch((error: any) => {
+      return error;
+    });
 
-    return venue;
+  return venue;
 }
--- schemas/venuesSchemas.ts
+++ schemas/venuesSchemas.ts
@@ -1,20 +1,20 @@
 export const venueSchema: any = {
-    title: "Data",
-    description: "Data venue schema",
-    type: "object",
-    properties: {
-        id: {type: "integer"},
-        name: {type: "string"},
-        surface: {type: "string", "nullable": true},
-        address: {type: "string", "nullable": true},
-        city: {type: "string"},
-        capacity: {type: "integer"},
-        image_path: {type: "string"},
-        coordinates: {type: "string"}
-    }
-}
+  title: "Data",
+  description: "Data venue schema",
+  type: "object",
+  properties: {
+    id: { type: "integer" },
+    name: { type: "string" },
+    surface: { type: "string", nullable: true },
+    address: { type: "string", nullable: true },
+    city: { type: "string" },
+    capacity: { type: "integer" },
+    image_path: { type: "string", nullable: true },
+    coordinates: { type: "string" },
+  },
+};
 
 export const venuesSchema: any = {
-    type: "array",
-    items: venueSchema
-}
+  type: "array",
+  items: venueSchema,
+};
