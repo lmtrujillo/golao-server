4d5c5d9379570ebfc2c9216334cea2e370df5c59
Luis Mario <luis.mario.trujillo@live.com.mx>
2022-01-28 13:10:50 -0600

    Indent and types

--- api/functions/teamsApi.ts
+++ api/functions/teamsApi.ts
@@ -1,103 +1,117 @@
-const fetch = require('node-fetch');
-import dotenv from 'dotenv'
-dotenv.config()
-import Ajv from 'ajv'
-const ajv = new Ajv()
-import { teamSchema, teamsSchema, teamsBySeasonSchema, leaguesByTeamDataSchema, teamsByCountrySchema } from '../../schemas/teamsSchemas'
+const fetch = require("node-fetch");
+import dotenv from "dotenv";
+dotenv.config();
+import Ajv from "ajv";
+const ajv = new Ajv();
+import {
+  teamSchema,
+  teamsSchema,
+  teamsBySeasonSchema,
+  leaguesByTeamDataSchema,
+  teamsByCountrySchema,
+} from "../../schemas/teamsSchemas";
 
 const api_key_token = process.env.API_TOKEN_PARAM! + process.env.API_KEY!;
 
 // GET N TEAM
 export async function getTeam(team_id: any): Promise<any> {
-    var endpoint = process.env.API_URL! + "teams/" + team_id + api_key_token;
-    var teams = await fetch(endpoint, {method: 'GET'})
-        .then((response: any) => response.json())
-        .then((res: any) => {
-            return (ajv.validate(teamSchema, res.data) ? res.data : ajv.errors);
-        })
-        .catch((error: any) => {
-            return error;
-        });
-    
-    return teams;
-}
+  var endpoint = process.env.API_URL! + "teams/" + team_id + api_key_token;
+  var teams = await fetch(endpoint, { method: "GET" })
+    .then((response: any) => response.json())
+    .then((res: any) => {
+      return ajv.validate(teamSchema, res.data) ? res.data : ajv.errors;
+    })
+    .catch((error: any) => {
+      return error;
+    });
 
+  return teams;
+}
 
 // GET N TEAM BY NAME
 export async function getTeamByName(team_name: any): Promise<any> {
-    var endpoint = process.env.API_URL! + "teams/search/" + team_name + api_key_token;
-    var teams = await fetch(endpoint, {method: 'GET'})
-        .then((response: any) => response.json())
-        .then((res: any) => {
-            return (ajv.validate(teamsSchema, res.data) ? res.data : ajv.errors);
-        })
-        .catch((error: any) => {
-            return error;
-        });
-    
-    return teams;
+  var endpoint =
+    process.env.API_URL! + "teams/search/" + team_name + api_key_token;
+  var teams = await fetch(endpoint, { method: "GET" })
+    .then((response: any) => response.json())
+    .then((res: any) => {
+      return ajv.validate(teamsSchema, res.data) ? res.data : ajv.errors;
+    })
+    .catch((error: any) => {
+      return error;
+    });
+
+  return teams;
 }
-
-
 
 // GET TEAMS BY SEASON
 export async function getTeamsBySeason(season_id: any): Promise<any> {
-    var endpoint = process.env.API_URL! + "teams/season/" + season_id + api_key_token;
-    var teams = await fetch(endpoint, {method: 'GET'})
-        .then((response: any) => response.json())
-        .then((res: any) => {
-            return (ajv.validate(teamsBySeasonSchema, res.data) ? res.data : ajv.errors);
-        })
-        .catch((error: any) => {
-            return error;
-        });
-    
-    return teams;
+  var endpoint =
+    process.env.API_URL! + "teams/season/" + season_id + api_key_token;
+  var teams = await fetch(endpoint, { method: "GET" })
+    .then((response: any) => response.json())
+    .then((res: any) => {
+      return ajv.validate(teamsBySeasonSchema, res.data)
+        ? res.data
+        : ajv.errors;
+    })
+    .catch((error: any) => {
+      return error;
+    });
+
+  return teams;
 }
 
 // GET TEAMS BY COUNTRY
 export async function getTeamsByCountry(country_id: any): Promise<any> {
-    var endpoint = process.env.API_URL! + "countries/" + country_id + "/teams" + api_key_token;
-    var teams = await fetch(endpoint, {method: 'GET'})
-        .then((response: any) => response.json())
-        .then((res: any) => {
-            return (ajv.validate(teamsByCountrySchema, res.data) ? res.data : ajv.errors);
-        })
-        .catch((error: any) => {
-            return error;
-        });
-    
-    return teams;
-}
+  var endpoint =
+    process.env.API_URL! + "countries/" + country_id + "/teams" + api_key_token;
+  var teams = await fetch(endpoint, { method: "GET" })
+    .then((response: any) => response.json())
+    .then((res: any) => {
+      return ajv.validate(teamsByCountrySchema, res.data)
+        ? res.data
+        : ajv.errors;
+    })
+    .catch((error: any) => {
+      return error;
+    });
 
+  return teams;
+}
 
 // GET CURRENT LEAGUES BY TEAM
 export async function getCurrentLeaguesByTeam(team_id: any): Promise<any> {
-    var endpoint = process.env.API_URL! + "teams/" + team_id + "/current" + api_key_token;
-    var teams = await fetch(endpoint, {method: 'GET'})
-        .then((response: any) => response.json())
-        .then((res: any) => {
-            return (ajv.validate(leaguesByTeamDataSchema, res.data) ? res.data : ajv.errors);
-        })
-        .catch((error: any) => {
-            return error;
-        });
-    
-    return teams;
-}
+  var endpoint =
+    process.env.API_URL! + "teams/" + team_id + "/current" + api_key_token;
+  var teams = await fetch(endpoint, { method: "GET" })
+    .then((response: any) => response.json())
+    .then((res: any) => {
+      return ajv.validate(leaguesByTeamDataSchema, res.data)
+        ? res.data
+        : ajv.errors;
+    })
+    .catch((error: any) => {
+      return error;
+    });
 
+  return teams;
+}
 
 // GET ALL LEAGUES BY TEAM
 export async function getLeaguesByTeam(team_id: any): Promise<any> {
-    var endpoint = process.env.API_URL! + "teams/" + team_id + "/history" + api_key_token;
-    var teams = await fetch(endpoint, {method: 'GET'})
-        .then((response: any) => response.json())
-        .then((res: any) => {
-            return (ajv.validate(leaguesByTeamDataSchema, res.data) ? res.data : ajv.errors);
-        })
-        .catch((error: any) => {
-            return error;
-        });
-    
-    return teams;
+  var endpoint =
+    process.env.API_URL! + "teams/" + team_id + "/history" + api_key_token;
+  var teams = await fetch(endpoint, { method: "GET" })
+    .then((response: any) => response.json())
+    .then((res: any) => {
+      return ajv.validate(leaguesByTeamDataSchema, res.data)
+        ? res.data
+        : ajv.errors;
+    })
+    .catch((error: any) => {
+      return error;
+    });
+
+  return teams;
 }
--- schemas/teamsSchemas.ts
+++ schemas/teamsSchemas.ts
@@ -1,135 +1,135 @@
 export const teamSchema: any = {
-    title: "Data",
-    description: "Data season schema",
-    type: "object",
-    properties: {
-        id: {type: "integer"},
-        legacy: {type: "integer"},
-        name: {type: "string"},
-        short_code: {type: "string"},
-        twitter: {type: "string", "nullable": true},
-        country_id: {type: "integer"},
-        national_team: {type: "boolean"},
-        founded: {type: "integer"},
-        logo_path: {type: "string"},
-        venue_id: {type: "integer"},
-        current_season_id: {type: "integer"}
-    }
-}
+  title: "Data",
+  description: "Data season schema",
+  type: "object",
+  properties: {
+    id: { type: "integer" },
+    legacy: { type: "integer", nullable: true },
+    name: { type: "string" },
+    short_code: { type: "string", nullable: true },
+    twitter: { type: "string", nullable: true },
+    country_id: { type: "integer" },
+    national_team: { type: "boolean" },
+    founded: { type: "integer" },
+    logo_path: { type: "string" },
+    venue_id: { type: "integer", nullable: true },
+    current_season_id: { type: "integer" },
+  },
+};
 
 export const teamBySeasonSchema: any = {
-    title: "Data",
-    description: "Data season schema",
-    type: "object",
-    properties: {
-        id: {type: "integer"},
-        legacy: {type: "integer"},
-        name: {type: "string"},
-        short_code: {type: "string", "nullable": true},
-        twitter: {type: "string", "nullable": true},
-        country_id: {type: "integer"},
-        national_team: {type: "boolean"},
-        founded: {type: "integer"},
-        logo_path: {type: "string"},
-        venue_id: {type: "integer"},
-        current_season_id: {type: "integer"},
-        is_placeholder: {type: "boolean"},
-    }
-}
+  title: "Data",
+  description: "Data season schema",
+  type: "object",
+  properties: {
+    id: { type: "integer" },
+    legacy: { type: "integer" },
+    name: { type: "string" },
+    short_code: { type: "string", nullable: true },
+    twitter: { type: "string", nullable: true },
+    country_id: { type: "integer" },
+    national_team: { type: "boolean" },
+    founded: { type: "integer" },
+    logo_path: { type: "string" },
+    venue_id: { type: "integer", nullable: true },
+    current_season_id: { type: "integer" },
+    is_placeholder: { type: "boolean" },
+  },
+};
 
 export const teamByCountrySchema: any = {
-    title: "Data",
-    description: "Data country schema",
-    type: "object",
-    properties: {
-        id: {type: "integer"},
-        legacy_id: {type: "integer", "nullable": true},
-        name: {type: "string"},
-        short_code: {type: "string", "nullable": true},
-        twitter: {type: "string", "nullable": true},
-        country_id: {type: "integer"},
-        national_team: {type: "boolean"},
-        founded: {type: "integer"},
-        logo_path: {type: "string"},
-        venue_id: {type: "integer"},
-        current_season_id: {type: "integer", "nullable": true},
-        is_placeholder: {type: "boolean"},
-    }
-}
+  title: "Data",
+  description: "Data country schema",
+  type: "object",
+  properties: {
+    id: { type: "integer" },
+    legacy_id: { type: "integer", nullable: true },
+    name: { type: "string" },
+    short_code: { type: "string", nullable: true },
+    twitter: { type: "string", nullable: true },
+    country_id: { type: "integer" },
+    national_team: { type: "boolean" },
+    founded: { type: "integer" },
+    logo_path: { type: "string" },
+    venue_id: { type: "integer", nullable: true },
+    current_season_id: { type: "integer", nullable: true },
+    is_placeholder: { type: "boolean" },
+  },
+};
 
 export const teamsSchema: any = {
-    type: "array",
-    items: teamSchema
-}
+  type: "array",
+  items: teamSchema,
+};
 
 export const teamsBySeasonSchema: any = {
-    type: "array",
-    items: teamBySeasonSchema
-}
+  type: "array",
+  items: teamBySeasonSchema,
+};
 
 export const teamsByCountrySchema: any = {
-    type: "array",
-    items: teamByCountrySchema
-}
+  type: "array",
+  items: teamByCountrySchema,
+};
 
 export const coverageSchema: any = {
-    title: "Data",
-    description: "Data leagues by team schema",
-    type: "object",
-    properties: {
-        predictions: {type: "boolean"},
-        topscorer_goals: {type: "boolean"},
-        topscorer_assists: {type: "boolean"},
-        topscorer_cards: {type: "boolean"},
-    }
-}
+  title: "Data",
+  description: "Data leagues by team schema",
+  type: "object",
+  properties: {
+    predictions: { type: "boolean" },
+    topscorer_goals: { type: "boolean" },
+    topscorer_assists: { type: "boolean" },
+    topscorer_cards: { type: "boolean" },
+  },
+};
 
 export const leagueByTeamDataSchema: any = {
-    title: "Data",
-    description: "Data leagues by team schema",
-    type: "object",
-    properties: {
-        id: {type: "integer"},
-        active: {type: "boolean"},
-        type: {type: "string"},
-        legacy: {type: "integer"},
-        country_id: {type: "integer"},
-        logo_path: {type: "string"},
-        name: {type: "string"},
-        is_cup: {type: "boolean"},
-        current_season_id: {type: "integer"},
-        current_round_id: {type: "integer"},
-        current_stage_id: {type: "integer"},
-        live_standings: {type: "boolean"},
-        coverage: coverageSchema,
-    }
-}
+  title: "Data",
+  description: "Data leagues by team schema",
+  type: "object",
+  properties: {
+    id: { type: "integer" },
+    active: { type: "boolean" },
+    type: { type: "string" },
+    legacy: { type: "integer" },
+    country_id: { type: "integer" },
+    logo_path: { type: "string" },
+    name: { type: "string" },
+    is_cup: { type: "boolean" },
+    current_season_id: { type: "integer" },
+    current_round_id: { type: "integer" },
+    current_stage_id: { type: "integer" },
+    live_standings: { type: "boolean" },
+    coverage: coverageSchema,
+  },
+};
 
 export const leagueByTeamSchema: any = {
-    title: "Data",
-    description: "Data leagues by team schema",
-    type: "object",
-    properties: {
-        data: leagueByTeamDataSchema,
-    }
-}
+  title: "Data",
+  description: "Data leagues by team schema",
+  type: "object",
+  properties: {
+    data: leagueByTeamDataSchema,
+  },
+};
 
 export const leaguesByTeamSchema: any = {
-    title: "Data",
-    description: "Data leagues by team schema",
-    type: "object",
-    properties: {
-        id: {type: "integer"},
-        name: {type: "string"},
-        league_id: {type: "integer"},
-        is_current_season: {type: "boolean"},
-        current_round_id: {type: "integer", "nullable": true},
-        current_stage_id: {type: "integer", "nullable": true},
-        league: leagueByTeamSchema
-    }
-}
+  title: "Data",
+  description: "Data leagues by team schema",
+  type: "object",
+  properties: {
+    id: { type: "integer" },
+    name: { type: "string" },
+    league_id: { type: "integer" },
+    is_current_season: { type: "boolean" },
+    current_round_id: { type: "integer", nullable: true },
+    current_stage_id: { type: "integer", nullable: true },
+    league: leagueByTeamSchema,
+  },
+};
 
 export const leaguesByTeamDataSchema: any = {
-    type: "array",
-    items: leaguesByTeamSchema
-}
+  type: "array",
+  items: leaguesByTeamSchema,
+};
